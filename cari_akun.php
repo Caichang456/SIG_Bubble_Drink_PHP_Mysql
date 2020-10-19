4ecc5ffbdf7f3bf4ed6ce61bc3d0761efbe04f00
Candra <liaocandra888@gmail.com>
2020-10-19 17:50:04 +0700

    cari_akun.php selesai

--- cari_akun.php
+++ cari_akun.php
@@ -10,7 +10,7 @@
 		<a href="cari_komentar_dan_rating.php">Komentar dan Rating</a> |
 		<a href="profil.php">Profil</a>
 		<h1>Cari Akun</h1>
-		<form action="akun.php" action="POST">
+		<form method="POST">
 			<input type="type" name="txt_cari_akun" placeholder="Cari Apa?">
 			<input type="submit" name="submit" value="Cari">
 		</form>
@@ -26,14 +26,14 @@
 				<th>Blokir</th>
 				<th>Aksi</th>
 			</tr>
-			<tr>
-				<?php
-					include "koneksi.php";
+			<?php
+				include "koneksi.php";
+				if(!isset($_POST['submit'])){
 					$data=mysqli_query($koneksi,"select * from tb_user");
 					while($d=mysqli_fetch_array($data)){ ?>
 						<tr>
 							<td><?php echo $d['id_user']; ?></td>
-							<td><?php echo $d['nama_unik']; ?></td>
+							<td><?php echo $d['user_name']; ?></td>
 							<td><?php echo $d['nama_depan']; ?></td>
 							<td><?php echo $d['nama_tengah']; ?></td>
 							<td><?php echo $d['nama_belakang']; ?></td>
@@ -42,14 +42,33 @@
 							<td><?php echo $d['bulan_lahir']; ?></td>
 							<td><?php echo $d['tahun_lahir']; ?></td>
 							<td><?php echo $d['blokir']; ?></td>
+							<td><?php echo $d['status']; ?></td>
 							<td>
 								<a href="blokir_akun.php?id_user=<?php echo $d['id_user']; ?>">Blokir</a>
 							</td>
 						</tr>
-					<?php 
-					}	
-				?>
-			</tr>
+					<?php } } ?>
+					<?php if(isset($_POST['submit'])){
+						$cari_akun=$_POST['txt_cari_akun'];
+						$data=mysqli_query($koneksi,"select * from tb_user where user_name like '%$cari_akun%'");
+						while($d=mysqli_fetch_array($data)){ ?>
+							<tr>
+								<td><?php echo $d['id_user']; ?></td>
+								<td><?php echo $d['user_name']; ?></td>
+								<td><?php echo $d['nama_depan']; ?></td>
+								<td><?php echo $d['nama_tengah']; ?></td>
+								<td><?php echo $d['nama_belakang']; ?></td>
+								<td><?php echo $d['hobby']; ?></td>
+								<td><?php echo $d['tanggal_lahir']; ?></td>
+								<td><?php echo $d['bulan_lahir']; ?></td>
+								<td><?php echo $d['tahun_lahir']; ?></td>
+								<td><?php echo $d['blokir']; ?></td>
+								<td><?php echo $d['status']; ?></td>
+								<td>
+									<a href="blokir_akun.php?id_user=<?php echo $d['id_user']; ?>">Blokir</a>
+								</td>
+						</tr>
+					<?php } }?> 
 		</table>
 	</body>
 </html>
