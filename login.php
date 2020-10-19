ca1d8576d5052949573ed03ba9a3b9d5643df972
Candra <liaocandra888@gmail.com>
2020-10-19 17:02:22 +0700

    Perubahan sikit pada login.php

--- login.php
+++ login.php
@@ -8,9 +8,18 @@
 		$data=mysqli_query($koneksi,"SELECT * FROM tb_user WHERE email='$email' and password='$password'");
 		$cek=mysqli_num_rows($data);
 		if($cek>0){
-			$_SESSION['txt_email']=$email;
-			$_SESSION['txt_password']=$password;
-			header("location:cari_akun.php");
+			$status=mysqli_query($koneksi,"SELECT * FROM tb_user WHERE status='Admin'");
+			$cek2=mysqli_num_rows($status);
+			if($cek2>0){
+				$_SESSION['txt_email']=$email;
+				$_SESSION['txt_password']=$password;
+				header("location:cari_akun.php");	
+			}
+			else{
+				$_SESSION['txt_email']=$email;
+				$_SESSION['txt_password']=$password;
+				header("location:cari_bubble_drink_user.php");
+			}
 		}
 		else{
 			header("location:index.php");
