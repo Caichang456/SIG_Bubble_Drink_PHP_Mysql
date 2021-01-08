2ede6594b4ce2c0161b48b340cefc3025ea790b2
Candra <liaocandra888@gmail.com>
2021-01-08 18:19:03 +0700

    Perbaikan3

--- logout.php
+++ logout.php
@@ -1,5 +1,12 @@
 <?php
 	session_start();
+	$_SESSION['txt_email']='';
+	$_SESSION['txt_password']='';
+	$_SESSION['txt_user_name']='';
+	unset($_SESSION['txt_email']);
+	unset($_SESSION['txt_password']);
+	unset($_SESSION['txt_user_name']);
+	session_unset();
 	session_destroy();
 	header("location:index.php");
 ?>
